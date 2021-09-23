5f6482448cd7c98950967fc1b6cad80fd6893f50
Crowdbotics <team@crowdbotics.com>
2021-09-23 07:30:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "we2sports_30768Identifier",
+  url: "https://we2sports_30768Identifier.botics.co",
+  api: "https://we2sports_30768Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
